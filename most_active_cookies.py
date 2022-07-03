8d1364236dfce3438a7038bb9960cb5529a5c644
Abhinav <in.abhi9@gmail.com>
2022-07-03 21:40:41 +0200

    chore: add most active cookie entry point

---
+++ most_active_cookies.py
@@ -0,0 +1,20 @@
+import argparse
+
+from datasource import CSVDatasource
+from lib import CookieProcessor
+from lib.output_util import OutputUtil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--day', required=True,
+                    help='Date to find the most active cookie for; in ISO format. Eg: 2020-02-24')
+parser.add_argument('-f', '--file', required=True,
+                    help='CSV file path. See ./tests/fixtures/sample.csv for example.')
+parser.add_argument('--ignore-error', default=True, action='store_true',
+                    help='Continue processing for invalid rows. Default to True')
+
+args = parser.parse_args()
+
+csv_datasource = CSVDatasource(args.file, ignore_error=args.ignore_error)
+OutputUtil.write_new_line_to_console(
+    CookieProcessor(csv_datasource).get_most_active_cookies(args.day)
+)
